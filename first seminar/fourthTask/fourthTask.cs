1aedc384e2302b49bd4ea3e0affb9fdc479a3f74
Vladimir <hahapupkin@gmail.com>
2022-09-03 22:12:21 +0400

    complete homework seminar two

--- first seminar/fourthTask/fourthTask.cs
+++ first seminar/fourthTask/fourthTask.cs
@@ -2,6 +2,6 @@ Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 2; i <= a; i += 2)
-{
-    Console.WriteLine(i);
-}
+    {
+        Console.WriteLine(i);
+    }
