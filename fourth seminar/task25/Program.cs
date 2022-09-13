5e682cc7ea91252a61900dccd972e4b2b4bde4c4
Vladimir <hahapupkin@gmail.com>
2022-09-13 21:41:59 +0400

    fourth seminar homework

---
+++ fourth seminar/task25/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("enter the number you want to raise to a power: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("enter degree to which you want to raise the number: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+//Не могли бы вы дать какие нибудь ресурсы по математике, пожалуйста
+//а то я два часа думал почему у меня ничего не работало при exit = 0 *epic_facepalm*
+int exit = 1;
+for (int i = 0; i < secondNumber; i++)
+{
+    Console.WriteLine(exit *= number);
+}
---
+++ fourth seminar/task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+class program
+{
+    static void Main(String[] args)
+    {
+        //ограниченное число скучно, можно ведь то кол-во которое хочет пользователь :Ь
+
+        Console.Write("enter mass your array:\t");
+        int elementsCount = int.Parse(Console.ReadLine()!);
+        int[] newArray = new int[elementsCount];
+
+        for (int i = 0; i < newArray.Length; i++){
+            Console.Write($"enter elements of array for index {i}:\t");
+            newArray[i] = int.Parse(Console.ReadLine()!);
+        }
+
+        for (int i = 0; i < newArray.Length; i++){
+
+            Console.Write(newArray[i] + "\t");
+        }
+    }
+}
