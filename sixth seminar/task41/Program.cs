0578487cee3c7d16fedc3a065eb6e2d8e3f2bbc9
Your Name <hahapupkin@gmail.com>
2022-09-26 20:55:12 +0300

    hw sixth seminar

---
+++ sixth seminar/task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+class program
+{
+    static void Main(String[] args)
+    {
+
+        int number;
+        
+        Console.WriteLine("Enter the number of array elements: \t");
+        number = Convert.ToInt16(Console.ReadLine());
+        int[] array = new int[number];
+
+        int countOfNumber = 0;
+
+        while (countOfNumber < number)
+        {
+            Console.WriteLine($"Enter number [{countOfNumber}]:");
+            array[countOfNumber] = int.Parse(Console.ReadLine());
+            countOfNumber++;
+        }
+
+        for (countOfNumber = 0; countOfNumber < number; countOfNumber++)
+        {
+            Console.WriteLine($"[{countOfNumber}]:\t " + array[countOfNumber]);
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"sum of positive array elements:\t {count}");
+        Console.ReadKey();
+    }
+}
---
+++ sixth seminar/task43/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, 
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//  b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+class program
+{
+    static void Main(String[] args)
+    {
+        string[] StringData = { "b1", "k1", "b2", "k2" };
+        double[] doubleData = new double[4];
+        //из семинара дядька говорил сделать заполнение функцией, ну я пытался
+        for (int i = 0; i < doubleData.Length; i++)
+        {
+            Console.Write(StringData[i] + ":\t");                //долго пытался превратить это в метод, 
+            doubleData[i] = double.Parse(Console.ReadLine()!);            //но как в данном случае работает return так и не понял
+        }
+       
+        if (doubleData[2] == doubleData[0] && doubleData[1] == doubleData[3])
+        {
+            Console.WriteLine("|");
+        }
+        else if (doubleData[1] == doubleData[0])
+        {
+            Console.WriteLine("||");
+        }
+        else
+        {
+            double x = (doubleData[2] - doubleData[0]) / (doubleData[1] - doubleData[3]);
+            double y = (doubleData[1] * x + doubleData[0]);
+            Console.WriteLine($"explose in coordinates: \t {x:F3} ; {y:F3}");
+        }
+    }
+
+}
+
+
